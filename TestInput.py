4ba30430c766dd4143f8c1d08a363ecf4e069315
Tthemiya <119820455+Tthemiya@users.noreply.github.com>
2022-12-29 00:19:23 +0530

    initialize TestInput.py

---
+++ TestInput.py
@@ -0,0 +1,11 @@
+# import numpy as np
+import pandas as pd
+
+# input1=pd.read_csv("sam.csv")
+# print(input1)
+
+# lec_detail=pd.read_csv("Data\instructors1.csv")
+# print(lec_detail)
+#
+# room_detail=pd.read_csv("Data\\room_table.csv")
+# print(room_detail)
