7566612a988a1c3e705d2478f5a42ab4c6d96bfe
99lakshan <charithlhirimuthugoda@gmail.com>
2023-01-25 22:12:48 +0530

    removed unnecessary file and updated

--- GetCapacity.py
+++ GetCapacity.py
@@ -7,7 +7,7 @@ def capacity(sub_code):  # 1
     cap = pd.read_csv("Data/2nd_Sem_Course_codes.csv")
     filtered_cap = cap.loc[cap['Course_Code'] == sub_code]
     value = filtered_cap.iloc[0]['capacity']
-    print("xxxxxxxxxxxxxxxxxxxxxxxxxxxxx  ", value)
+    #print("xxxxxxxxxxxxxxxxxxxxxxxxxxxxx  ", value)
 
     return value
 
@@ -16,7 +16,7 @@ def block(sub_code):  # 2
     blocktype = pd.read_csv("Data/2nd_Sem_Course_codes.csv")
     filtered_blocktype = blocktype.loc[blocktype['Course_Code'] == sub_code]
     value1 = filtered_blocktype.iloc[0]['block_type']
-    print("@@@@@@@@@@@@@@@@@@@@@@@@@@", value1)
+    #print("@@@@@@@@@@@@@@@@@@@@@@@@@@", value1)
 
     return value1
 
@@ -43,7 +43,7 @@ def room(cap, blockType, pp):  # 3
 
         if x >= y:
             value3 = store[j]
-            print("capacity of selected course : ", y, " capacity of lecture hall : ", x)
+           # print("capacity of selected course : ", y, " capacity of lecture hall : ", x)
             given_room_cap.append(x)
             given_room.append(value3)
 
@@ -70,8 +70,8 @@ def sort(book_room_L1, book_room_L2):  # 4
                 book_room_L2[j] = c
                 book_room_L1[j] = d
 
-    print("book room no. : ", book_room_L1)
-    print("book room cap : ", book_room_L2, "\n")
+    #print("book room no. : ", book_room_L1)
+    #print("book room cap : ", book_room_L2, "\n")
 
     #storing = store(book_room_L1, book_room_L2, detail_row, pp)
     #print(storing)
