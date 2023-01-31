ff56afae007cc2787837c44e3ee302b366fbe261
Lakshan Hirimuthugodage <119588937+99lakshan@users.noreply.github.com>
2023-01-31 09:27:14 +0530

    updated

--- Enterinputs.py
+++ Enterinputs.py
@@ -1,5 +1,3 @@
-# this file is not nessosery bcz if we type our input data in csv file there is no need to use this.
-
 import pandas as pd
 
 CC_list = ['CC_CODE']
@@ -7,7 +5,6 @@ LC_list = ['LEC_CODE']
 TS1_list = ['TIME_SLOT1']
 TS2_list = ['TIME_SLOT2']
 TS3_list = ['TIME_SLOT3']
-
 
 def inputs(Answer):
     day = pd.read_csv("Data/Day_arrangement.csv")
@@ -30,11 +27,8 @@ def inputs(Answer):
         TS2_list.append(TS2)
         TS3 = str(input("Enter your Preferred 3rd time slot : "))
         TS3_list.append(TS3)
-    #        TS4 = str(input("Enter your Preferred 4th time slot : "))
-    #        TS4_list.append(TS4)
 
     Input_table = [CC_list, LC_list, TS1_list, TS2_list, TS3_list]
-    # print(Input_table)
 
     DF = pd.DataFrame(Input_table)
     new_DF = DF.T
@@ -43,5 +37,11 @@ def inputs(Answer):
     headers = new_DF.iloc[0]
     new_DF = pd.DataFrame(new_DF.values[1:], columns=headers)
 
-    new_DF.to_csv("input2.csv", index=False)
+    try:
+        existing_df = pd.read_csv("input2.csv")
+        combined_df = pd.concat([existing_df, new_DF], ignore_index=True)
+        combined_df.to_csv("input2.csv", index=False)
+    except FileNotFoundError:
+        new_DF.to_csv("input2.csv", index=False)
+
     return new_DF
